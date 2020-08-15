04705525aa3aaf2a1e0d48dedcb2bd71cf14be90
OZKAN S <ozkans@OZKANs-MacBook-Pro.local>
2020-08-15 00:19:06 -0500

    file writer program

--- Chapter4/assignment2.py
+++ Chapter4/assignment2.py
@@ -21,5 +21,51 @@ class WriteFile(object):
 
 class DelimFile(WriteFile):
     
-    def __init__(self, filename, delim):
-        super(DelimFile, self).__init__
+    def __init__(self, filename, delim): # delimfile needs a type of delimeter to be specified
+        super(DelimFile, self).__init__(filename) # delimfile need a file name so this will do
+        self.delim = delim
+
+    '''polymorphisim: here we use the same interface (write method) to do similar things'''
+    def write(self, alist):
+        line = self.delim.join(alist)
+        self.write_line(line)
+
+class LogFile(WriteFile):
+
+    def write(self, aline):
+        dt = datetime.now()
+        date_str = dt.strftime('%Y-%m-%d %H:%M')
+        self.write_line('{}  {}'.format(date_str, aline))
+
+
+        
+
+
+
+# test it now
+log = LogFile(filename='firstlog.txt')
+csv_file = DelimFile(filename='firstdata.csv', delim=',')
+
+# polymorphism in acton!
+text1 = 'Ah it seems to be working!'
+text2 = 'This is just a test!'
+log.write(text1)
+log.write(text2)
+
+
+header = ['header1', 'heade2', 'header3', 'header4']
+csv_file.write(header)
+
+text3 = 'header is written'
+log.write(text3)
+
+first_row = ['entry1', 'entry2', 'entry3', 'entry4']
+csv_file.write(first_row)
+
+text4 = 'first row of the file is written'
+log.write(text4)
+text5 = 'work is complete'
+log.write(text5)
+
+
+
